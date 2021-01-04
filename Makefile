0195f0f9595d04b06bdf580e8f41cc6387cd89e8
yulia633 <ignat633yulia@yandex.ru>
2021-01-04 22:13:46 +0300

    add test

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ validate:
 	composer validate
 
 lint:
-	composer exec --verbose phpcs -- --standard=PSR12 bin
+	composer exec --verbose phpcs -- --standard=PSR12 bin src tests
 
-gendiff:
-	./bin/gendiff -h
+test:
+	composer run-script phpunit tests
---
+++ tests/GenDiffTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Differ\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Differ\Differ\genDiff;
+
+class ParserTest extends TestCase
+{
+    public function testDiffJsonFlat()
+    {
+        $diff = genDiff(__DIR__ . '/fixtures/file1.json', __DIR__ . '/fixtures/file2.json');
+
+        $this->assertStringEqualsFile(__DIR__ . '/fixtures/diff.txt', $diff);
+    }
+}
