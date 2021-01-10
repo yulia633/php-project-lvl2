f3da8cab592502ab75639409daf2f20e0292ada1
yulia633 <ignat633yulia@yandex.ru>
2021-01-10 18:43:01 +0300

    fixed parser: add type yml

--- src/parsers.php
+++ src/parsers.php
@@ -9,6 +9,7 @@ function parse($data, $type)
     switch ($type) {
         case 'json':
             return json_decode($data, true);
+        case 'yml':
         case 'yaml':
             return Yaml::parse($data);
         default:
