4371aef9764a6d6de556f2c6c5657ed049fe4c17
yulia633 <ignat633yulia@yandex.ru>
2021-02-17 19:07:16 +0300

    update test: add description in dataProvider

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -24,12 +24,12 @@ class GenDiffTest extends TestCase
     public function additionProvider()
     {
         return [
-            ["diffJson.txt", "file1.json", "file2.json", "json"],
-            ["diffJson.txt", "file1.yml", "file2.yml", "json"],
-            ["diff.txt", "file1.json", "file2.json", "stylish"],
-            ["diff.txt", "file1.yml", "file2.yml", "stylish"],
-            ["diffStylish.txt", "fileNest1.json", "fileNest2.json", "stylish"],
-            ["diffStylish.txt", "fileNest1.yml", "fileNest2.yml", "stylish"]
+            'input flat json - output json' => ["diffJson.txt", "file1.json", "file2.json", "json"],
+            'input flat yml - output json' => ["diffJson.txt", "file1.yml", "file2.yml", "json"],
+            'input flat json - output stylish' => ["diff.txt", "file1.json", "file2.json", "stylish"],
+            'input flat yml - output stylish' => ["diff.txt", "file1.yml", "file2.yml", "stylish"],
+            'input nested json - output stylish' => ["diffStylish.txt", "fileNest1.json", "fileNest2.json", "stylish"],
+            'input nested yml - output stylish' => ["diffStylish.txt", "fileNest1.yml", "fileNest2.yml", "stylish"]
         ];
     }
 }
